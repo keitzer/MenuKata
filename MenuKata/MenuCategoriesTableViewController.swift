973f350a3ea434cae438f1fd50eff3be4343864d
Alex Ogorek <ogoalex57@gmail.com>
2017-04-13 22:30:11 -0400

    created table view controller

---
+++ MenuKata/MenuCategoriesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  MenuKata
+//
+//  Created by Alex Ogorek on 4/13/17.
+//  Copyright © 2017 Kata. All rights reserved.
+//
+
+import UIKit
+
+class MenuCategoriesTableViewController: UITableViewController {
+    
+}
